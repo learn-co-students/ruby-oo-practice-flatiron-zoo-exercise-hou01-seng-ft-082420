bfdf108acc269eb021859bd3ef61e8573f65ca84
IainLR <iainrobertzon@gmail.com>
2020-08-30 18:43:29 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
+    @@all = []
+    attr_accessor  :weight, :zoo
+    attr_reader :species, :nickname
+
+    def initialize (species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname 
+        @zoo = zoo
+        self.class.all << self
+    end 
+
+    def self.all 
+        @@all
+    end
+
+    def self.find_by_species (species)
+        @@all.select { |animal| animal.species == species}
+        #Animal.find_by_species("Buffalo")
+    end 
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Zoo
+    @@all = []
+    attr_accessor :name, :location 
 
+    def initialize (name, location)
+        @name = name
+        @location = location 
+        self.class.all << self
+    end 
+
+    def self.all 
+        @@all
+    end
+
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end 
+    def animal_species 
+        animals.map {|animal| animal.species}.uniq 
+    end
+
+    def find_by_species (species)
+        animals.select do |animals| 
+        animals.species == species
+        end 
+    end 
+
+    def animal_nicknames
+        animals.map {|animals| animals.nickname}
+    end 
+
+    def self.find_by_location (location)
+        @@all.select {|zoos| zoos.location == location}
+        #Zoo.find_by_location("Montreal")
+    end 
+    binding.pry
+    0
+    
 end
